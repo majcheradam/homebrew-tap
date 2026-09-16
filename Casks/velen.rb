f9335af3e94fef80876d92f5fb9b2296a99a5ac0
Adam Majcher <Adammajcher20@gmail.com>
2026-09-17 00:03:34 +0200

    Update Velen to 26.916.5

--- Casks/velen.rb
+++ Casks/velen.rb
@@ -1,8 +1,8 @@
 cask "velen" do
-  version "26.916.4"
-  sha256 "b672d52ba1bfc440367e6fca42c47b8396daae9220a935dc52699d4d3d7b18f9"
+  version "26.916.5"
+  sha256 "dbe927baedd59b084e6dbab226208cdba3da80909f20a30b32388265932cb2cc"
 
-  url "https://kdkwoxtmlhie85jd.public.blob.vercel-storage.com/downloads/Velen-26.916.4-qVp7XzMtRadOwZxyE1rEaM6uPLBDPL.zip"
+  url "https://kdkwoxtmlhie85jd.public.blob.vercel-storage.com/downloads/Velen-26.916.5-YurNA5PyEDltJ8GnjP6CDxtiTShx0X.zip"
   name "Velen"
   desc "Menu bar leaderboard for AI token usage"
   homepage "https://velen.sh/"
